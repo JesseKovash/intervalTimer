e06230df03f0ec46453734cc31f71cc1e452d7a2
Jesse Kovash <jessekovash@gmail.com>
2022-07-14 10:50:58 -0600

    minor changes made to server/index and server/server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
-
+import app from './server.js';
 import express from 'express';
-const app = express()
+// const app = express()
 const port = process.env.PORT || 80;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ server/server.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+module.exports = app;
