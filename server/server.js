c030975f7c987fdb9b8ee8f46ee1ec5aa63a6de9
Jesse Kovash <jessekovash@gmail.com>
2022-07-14 10:56:27 -0600

    added export expression to server/server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+export const app = express();
 
 app.use(express.static(path.join(__dirname, 'index.html')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-module.exports = app;
+// module.exports = app;
